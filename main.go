860b53f9a8dc8704e46ca581f9d8261a6730bf06
Mikada <panupong.mikada@gmail.com>
2024-03-27 00:51:56 +0700

    use godotenv read .env file

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"log"
+
 	"github.com/KKGo-Software-engineering/fun-exercise-api/postgres"
 	"github.com/KKGo-Software-engineering/fun-exercise-api/wallet"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 
 	_ "github.com/KKGo-Software-engineering/fun-exercise-api/docs"
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
-//	@title			Wallet API
-//	@version		1.0
-//	@description	Sophisticated Wallet API
-//	@host			localhost:1323
+// @title			Wallet API
+// @version		1.0
+// @description	Sophisticated Wallet API
+// @host			localhost:1323
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	p, err := postgres.New()
 	if err != nil {
 		panic(err)
--- postgres/postgres.go
+++ postgres/postgres.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -13,8 +14,8 @@ type Postgres struct {
 }
 
 func New() (*Postgres, error) {
-	databaseSource := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable", "localhost", 5432, "root", "password", "wallet")
+	databaseSource := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 	db, err := sql.Open("postgres", databaseSource)
 	if err != nil {
 		log.Fatal(err)
